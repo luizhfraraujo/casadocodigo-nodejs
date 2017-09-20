65c363ce214bdab4d7486c6b608c84e298ba2d11
Luiz Araújo <luiz.araujo@controlinformatica.com.br>
2017-09-20 17:34:31 -0300

    ADicionado Páginas de ERros

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -4,17 +4,17 @@ function createDBConnection(){
 
     if(process.env.NODE_ENV == 'development') {
         return mysql.createConnection({
-            
+            /* 
             host : 'strawberry.arvixe.com',
             user : 'node_testes',
             password : 'nodetestes',
             database : 'node_testes_bd',
-            
-            /* host : 'localhost',
+            */
+            host : 'localhost',
             user : 'node_testes',
             password : 'nodetestes',
-            database : 'casadocodigo_nodejs',
-            */
+            database : 'casadocodigo_nodejs22',
+            
         
         });
     };
@@ -22,16 +22,16 @@ function createDBConnection(){
 
     if(process.env.NODE_ENV == 'test') {
         return mysql.createConnection({
+            
+            host : 'strawberry.arvixe.com',
+            user : 'node_testes',
+            password : 'nodetestes',
+            database : 'node_testes_bd',
             /*
             host : 'strawberry.arvixe.com',
             user : 'node_testes',
             password : 'nodetestes',
-            database : 'node_testes_bd',
-            */
-            host : 'strawberry.arvixe.com',
-            user : 'node_testes',
-            password : 'nodetestes',
-            database : 'node_testes_bd',
+            database : 'node_testes_bd',*/
         
         });
     };
--- config/express.js
+++ config/express.js
@@ -13,10 +13,24 @@ module.exports = function() {
     app.use(bodyParser.json());
     app.use(expressValidator());
 
+   
+
     app.use(express.static('./app/public'));
     load('routes', {cwd: 'app'})
         .then('infra')
         .into(app);
+
+    app.use(function(req, res, next) {
+        res.status(404).render('errors/404');
+    });
+
+    app.use(function(error, req, res, next) {
+        if(process.env.NODE_ENV == 'production') {
+            res.status(500).render('errors/500');
+            return;
+        }
+        next(error);
+    });
 
     return app;
 }
